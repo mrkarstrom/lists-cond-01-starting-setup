befcd2658ef5f8659d3133d8263d197006c6e8dc
Magnus Karström <97023772+yeswebdotse@users.noreply.github.com>
2022-07-26 15:07:06 +0200

    Trying out some switches of my own

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ const app = Vue.createApp({
     return {
       addGoalValue: '',
       goals: [],
+      btn1Visible: true,
+      btn2Visible: true,
     };
   },
   methods: {
     addGoal() {
       this.goals.push(this.addGoalValue.value);
+    },
+    switchState() {
+      this.btn1Visible = !this.btn1Visible;
+      this.btn2Visible = !this.btn2Visible;
+    },
+    btn1Toggle() {
+      this.btn1Visible = !this.btn1Visible;
+    },
+    btn2Toggle() {
+      this.btn2Visible = !this.btn2Visible;
     },
   },
 });
